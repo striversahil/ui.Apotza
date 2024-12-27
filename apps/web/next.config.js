32f6ddc2d16fad25444b67daf4ff2c3f126bcea7
Sahil Kumar Jha <striversahil@gmail.com>
2024-12-27 12:46:56 +0530

    changed next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,10 @@
+/** @type {import('next').NextConfig} */
+
 import path from "path";
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    // Set alias for easier imports
     config.resolve.alias["@"] = path.resolve(__dirname);
-
-    // Add support for shared package
-    config.module.rules.push({
-      test: /\.(js|ts|jsx|tsx)$/,
-      include: [path.resolve(__dirname, "../../packages/ui_patterns")],
-      use: ["babel-loader"],
-    });
-
     return config;
   },
 };
