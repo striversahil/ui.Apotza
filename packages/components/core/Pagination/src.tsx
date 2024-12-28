0de9dd911d5ad870bc385c315b8b3db699a66b28
Sahil Kumar Jha <striversahil@gmail.com>
2024-12-28 07:40:38 +0530

    finalizing Pagination 1.0

--- packages/components/core/Pagination/src.tsx
+++ packages/components/core/Pagination/src.tsx
@@ -18,21 +18,43 @@ export const Index = ({
 
   const Click = (num: number) => {
     setCurrPage(num);
+    onPageChange(num);
   };
 
+  const Button = ({
+    prop,
+    slug,
+  }: {
+    prop: number;
+    slug: string;
+  }): React.ReactNode => (
+    <div className="flex items-center ">
+      <button
+        onClick={() => {
+          Click(currPage + prop);
+        }}
+        className="p-2 rounded-full bg-blue-500 mx-2 hover:bg-blue-700 "
+      >
+        {slug}
+      </button>
+    </div>
+  );
+
   return (
-    <div className="w-1/3 h-20 rounded-full flex justify-around bg-blue-400">
-      <button onClick={() => Click(currPage - 1)}>Left</button>
-      <div></div>
-      {[-1, 0, 1].map((add, idx) => (
-        <button
-          onClick={() => Click(currPage + add)}
-          className="h-full w-5 rounded-full bg-blue-300"
-        >
-          {currPage + add}
-        </button>
-      ))}
-      <button onClick={() => Click(currPage + 1)}>Right</button>
+    <div className="w-1/3 h-20 rounded-full flex justify-evenly bg-gradient-to-l from-blue-900 to-transparent border border-yellow-500">
+      <Button prop={-1} slug={"Left"} />
+      <div className="flex items-center justify-around w-full ">
+        {[-1, 0, 1].map((add, idx) => (
+          <button
+            onClick={() => Click(currPage + add)}
+            className="h-fit w-10  p-2 rounded-full text-blue-200 hover:bg-blue-300/55"
+            key={idx}
+          >
+            {currPage + add}
+          </button>
+        ))}
+      </div>
+      <Button prop={1} slug={"Right"} />
     </div>
   );
 };
