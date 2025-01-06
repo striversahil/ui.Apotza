cdff725ef84ba67e626054c95091e6d6ca34b364
Sahil Kumar Jha <striversahil@gmail.com>
2025-01-06 20:51:20 +0530

    Refined types, styling adjustments, and import optimization

--- packages/ui_patterns/Layout/Hero/code.tsx
+++ packages/ui_patterns/Layout/Hero/code.tsx
@@ -11,14 +11,14 @@ const Code = (props: Props) => {
 
   return (
     <div className="w-full flex justify-center">
-      <div className="relative w-1/2 max-h-[500px] rounded-3xl overflow-y-scroll">
+      <div className="relative w-1/2 max-h-[500px]  rounded-3xl overflow-y-scroll ">
         <div
           className="sticky z-50 top-5 w-20 left-full mx-5 rounded-xl bg-slate-600 p-2 text-center cursor-pointer hover:bg-slate-700"
           onClick={() => CopytoClickboard()}
         >
           {isCopied ? "Copied" : "Copy"}
         </div>
-        <div className="relative -top-10">
+        <div className="relative -top-10 ">
           <CodeBlock
             text={props.code}
             showLineNumbers={true}
